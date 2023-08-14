9add70b3df017b2efe9feeea52fda8691e0f8961
Eric Fallon <ericmfal@gmail.com>
2023-08-14 16:58:58 -0700

    Background image

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Main from '../src/components/Main'
+import Main from '../src/components/Main';
 
 function App() {
   return (
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Component } from 'react'
 import axios from 'axios'
+import background from '../images/mtrainier.jpg'
 
 const Main = () => {
     const [message, setMessage] = useState('...Loading')
@@ -10,8 +11,12 @@ const Main = () => {
     }, []); //empty brackets mean that it will only run once instead of every time the prop changes
 
     return (
-        <div style={{backgroundImage: "url(/images/mtrainier.jpg)"}}>
-
+        <div style={{backgroundImage: `url(${background})`,
+                    height: "100vh",
+                    width: "100vw",
+                    backgroundSize: "cover"
+        }}>
+            
         </div>
     )
 }
