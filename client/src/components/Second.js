0a44df1842dd4de6bf2d23c5b3aa720f09c4a4c8
Eric Fallon <ericmfal@gmail.com>
2023-09-04 16:10:38 -0700

    Simplified sticky nav

--- client/src/components/Second.js
+++ client/src/components/Second.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../secondary.css';
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 
 const Second = () => {
     
@@ -11,14 +12,17 @@ const Second = () => {
                 <h1 align="right">Ahoy, I'm <strong>Eric Fallon</strong></h1>
                 <h3 align="right">Web developer, brewer, climber, skier, <br></br>and resident insect/reptile expert</h3>
             </div>
-            <Navbar expand="lg" className="bg-body-tertiary navbar">
+            <Navbar sticky="top" expand="lg" className="bg-body-tertiary navbar">
                 <Nav.Link href='#Home' className='navlink'>Home</Nav.Link>
                 <Nav.Link href='#About' className='navlink'>About</Nav.Link>
                 <Nav.Link href='#Portfolio' className='navlink'>Portfolio</Nav.Link>
                 <Nav.Link href='#Contact' className='navlink'>Contact</Nav.Link>
             </Navbar>
             <div className='body-container home-container'>
-
+                <Container>
+                    <Container className='home-left'></Container>
+                    <Container className='home-right'></Container>
+                </Container>
             </div>
             <div className='body-container about-container'>
 
